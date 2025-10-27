c8b7d1e3466675487fd5e9588912c07217aeaadf
Cherie <pulcheriembaye11@gmail.com>
2025-10-27 19:27:31 -0400

    Added JavaDocs and comments

--- Player.java
+++ Player.java
@@ -5,19 +5,44 @@ public class Player {
     private final String name;
     private final ArrayList<Card> hand; //cards currently in the players hand
 
+    /**
+     * Constructs a new Player with the specified name and an empty hand.
+     *
+     * @param name the name of the player
+     */
+
     public Player(String name){
         this.name = name;
         this.hand = new ArrayList<>();
     }
 
+    /**
+     * Gets the player's name.
+     *
+     * @return the name of the player
+     */
+
     public String getName(){
         return name;
     }
+
+    /**
+     * Gets the list of cards currently in the player's hand.
+     *
+     * @return an ArrayList containing the player's cards
+     */
 
     // get the players hand, it will return a list of cards the player is holding
     public ArrayList<Card> getHand(){
         return hand;
     }
+    /**
+     * Displays the player's hand in the console.
+     * Each card is printed with its index for easier selection.
+     *
+     * @return a placeholder string (not used)
+     */
+
     // show players hand in the console
     public String showHand(){
         for(int i =0; i< hand.size(); i++){
@@ -26,15 +51,6 @@ public class Player {
         return " ";
     }
 
-
-    //public void drawCard(){
-        //the pile to be infinite
-        //GameLogic gamelogic = new GameLogic();
-       // hand.add(GameLogic.getTopCard());
-       //System.out.println(name + "drew a card: " + newCard);
-
-
-    //}
 
     //choosing the card to play
     public boolean playCard(int index, Card topCard, ArrayList<Card> discardPile){
@@ -47,6 +63,7 @@ public class Player {
          * @param index which card from the players hand example card 2
          * @param topCard the current top card on the discard pile
          * @param discardPile the list of all cards that have been played
+         * @return true if the card was played successfully, false otherwise
          */
         if(index < 0 || index >= hand.size()){
             System.out.println(("invalid card number"));
@@ -67,7 +84,12 @@ public class Player {
 
     }
 
-
+    /**
+     * Checks if the player has no cards left.
+     * Used to determine if the player has won the game.
+     *
+     * @return true if the player's hand is empty, {@code false} otherwise
+     */
     public boolean hasZeroCard(){
         if(hand.isEmpty()){
             return true;
