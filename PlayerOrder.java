251b2112d1b608ca787c60f45c3a7e9713a617a9
pardis-21 <pardisehsani@cmail.carleton.ca>
2025-10-27 19:30:39 -0400

    added javadocs to the playerorder class

--- PlayerOrder.java
+++ PlayerOrder.java
@@ -1,18 +1,31 @@
 import java.util.ArrayList;
 
+/**
+ * This class manages the player order implementation, because there are both SKIP and REVERSE cards
+ * a circular doubly linkedlist implementation was used. It keeps track of the current player, the previous
+ * player and the next player in the order by either moving clockwise or counterclock, depending on the game.
+ * This class also adds players in order and retrives them in order.
+ * @Author Pardis Ehsani 101300400
+ */
 public class PlayerOrder {
 
     private playerNode firstPlayer; //head
     private playerNode lastPlayer; //tail
     private int numPlayers; //num of nodes
     private playerNode currentPlayer;
-
     //creating a node
+
+    //creating a playerNode for CDLL
     private class playerNode {
         private Player pName; //data
         private playerNode prev;
         private playerNode next;
 
+        /**
+         * Creates a new Player node with the given player from player class
+         *
+         * @param p the player that will be stored as data in the node
+         */
         //constructor
         public playerNode(Player p) {
             this.pName = p;
@@ -22,6 +35,9 @@ public class PlayerOrder {
         }
     }
 
+    /**
+     * Inititializes an empty player order
+     */
     //constructor
     public PlayerOrder() {
         this.firstPlayer = null;
@@ -29,10 +45,21 @@ public class PlayerOrder {
         this.numPlayers = 0;
         this.currentPlayer = null;
     }
+
+    /**
+     * Checks if there are any players in the nodes/order
+     *
+     * @return true if there are no players that exist, false otherwise
+     */
     public boolean isEmpty() {
         return numPlayers == 0;
     }
 
+    /**
+     * Adds a player to a player node (circularly doubly linked-list style) when called upon to link to other player nodes
+     *
+     * @param p a player from the Player class to be addedd to the order
+     */
     //adding players to the linkedList
     public void addPlayer(Player p) {
         playerNode tempPlayerNode = new playerNode(p);
@@ -61,6 +88,9 @@ public class PlayerOrder {
         numPlayers++;
     }
 
+    /**
+     * Moves the next players turn with direction being clockwise
+     */
     //traversing forward the doubly linked list
     public void nextPlayerClockwise(){
         if (currentPlayer != null) {
@@ -68,6 +98,9 @@ public class PlayerOrder {
 
         }
     }
+    /**
+     * Moves the next players turn with direction being counterclockwise
+     */
     //traversing backwards the doubly linked list
     public void nextPlayerCounterClockwise(){
         if (currentPlayer != null) {
@@ -76,6 +109,11 @@ public class PlayerOrder {
 
     }
 
+    /**
+     * Returns all the players from the CDLL as an ArrayList
+     *
+     * @return list of players in order of added into CDLL
+     */
     public ArrayList<Player> getAllPlayersToArrayList() {
         ArrayList<Player> allPlayers = new ArrayList<>();
 
@@ -90,10 +128,11 @@ public class PlayerOrder {
         return allPlayers;
     }
 
-    public int getTotalNumPlayers(){
-        return numPlayers;
-    }
-
+    /**
+     * Returns the currents player turn in the game
+     *
+     * @return the current player in the game, if there are no players, then is null
+     */
     public Player getCurrentPlayer() {
        if (currentPlayer == null){
            return null;
@@ -102,7 +141,9 @@ public class PlayerOrder {
            return currentPlayer.pName;
        }
     }
-
+    /**
+     * Returns a string representation of the order of the PlayerOrder class
+     */
     @Override
     public String toString() {
         return super.toString();
